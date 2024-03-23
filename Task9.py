0784d17ba8e0f91d6326c4a5724a6bec37f8e74e
Seiko <wakirt67@gmail.com>
2024-03-23 12:54:15 +0300

    sem2

---
+++ Task9.py
@@ -0,0 +1,10 @@
+numbers =['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+print('Вышел зайчик погулять')
+print(' ')
+numbers =['Раз','Два','Три','Четыре','Пять']
+for number in numbers:
+    print(number)
+    print('Вышел зайчик погулять')
+
---
+++ sem 2_2
@@ -0,0 +1,13 @@
+my_var1 = 5
+my_var2 = 10
+
+def my_func():
+    my_var1 = 15
+    
+    print (my_var1)
+
+    print (my_var2)
+
+my_func()
+
+# Функция изменяет my_var1 и выводит, а my_var2 берет из глобальной переменной, не найдя ее внутри функции
